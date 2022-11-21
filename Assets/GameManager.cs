1f44cf4cda0ed38f2487d78541b037d13cdb8375
109153112 <118705365+109153112@users.noreply.github.com>
2022-11-21 16:27:49 +0800

    初次上傳
    第一次完整上傳

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,13 +8,14 @@ public class GameManager : MonoBehaviour
     GameObject car;              // 用來儲存汽車物件
     GameObject flag;             // 用來儲存旗子物件
     GameObject distance;         // 用來儲存UI文字物件
-    GameObject score;
+    GameObject Score;            //用來儲存分數文字物件
+
     void Start() // 將場景中的物件放到已宣告的各個物件中
     {
         car = GameObject.Find("car");
         flag = GameObject.Find("flag");
         distance = GameObject.Find("Distance");
-        score = GameObject.Find("score");
+        Score = GameObject.Find("Score");
     }
 
     void Update()
@@ -23,10 +24,14 @@ public class GameManager : MonoBehaviour
         float length = flag.transform.position.x - car.transform.position.x;
         // 將旗子與汽車的距離顯示在UI上面
         distance.GetComponent<Text>().text = "距離目標還有 " + length.ToString("F2") + "m";
+        //算分數
         float ScoreNumber = 100/length;
-        if (ScoreNumber<=0)
+        //將分數顯示在UI上面
+        if (ScoreNumber <= 0)
             ScoreNumber = 0;
-        score.GetComponent<Text>().text = "分數 " + length.ToString("F2") + "分數";
+        Score.GetComponent<Text>().text = "分數 " + length.ToString("F2") + "分";
+        
+       
 
     }
 }
